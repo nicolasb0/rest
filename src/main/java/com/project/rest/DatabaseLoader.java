8b62f214a1113f605ab2ad3ee6d479623f8197ea
DESKTOP-R23RV8A\User <nicob92@hotmail.fr>
2021-07-26 01:18:32 +0300

    Basic application

---
+++ src/main/java/com/project/rest/DatabaseLoader.java
@@ -0,0 +1,22 @@
+package com.project.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseLoader implements CommandLineRunner {
+	
+	private final PersonRepository repository;
+
+	@Autowired
+	public DatabaseLoader(PersonRepository repository) {
+		this.repository = repository;
+	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+		this.repository.save(new Person("John", "Smith"));
+	}
+
+}
---
+++ src/main/java/com/project/rest/PersonRepository.java
@@ -0,0 +1,7 @@
+package com.project.rest;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PersonRepository extends CrudRepository<Person, Long> {
+
+}
